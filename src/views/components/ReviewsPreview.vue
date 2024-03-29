16338bf7856aaf7f243e281b613797db016cf593
Lazar <celic96@gmail.com>
2024-03-29 11:33:36 +0100

    Review done for all screen size

--- src/views/components/ReviewsPreview.vue
+++ src/views/components/ReviewsPreview.vue
@@ -9,16 +9,16 @@ export default  {
 
 <template>
     <div class="section-reviews bg-black text-white py-12">
-        <div class="container mx-auto px-4">
+        <div class="flex flex-col mx-auto px-4">
             <div class="text-center mb-8">
             <h2 class="font-heading text-3xl uppercase">Reviews</h2>
             </div>
-            <div class="flex flex-row h-46 border border-white">
+            <div class="flex flex-row h-46 review-border review-medium sm:w-3/5">
                 <div class="w-1/3 sm:w-1/3">
                     <img src="/images/review.jpg">
                 </div>
-                <div class="flex flex-col w-2/3 font-body p-4 sm:w-2/3 sm:p-4">
-                    {{ '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.""' }}
+                <div class="w-2/3 p-4 sm:w-2/3 sm:p-4">
+                    <p class="text-sm md:text-base sm:text-3xl">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
                 </div>
             </div>
             <div class="text-center mt-8 sm:mt-12">
@@ -31,5 +31,18 @@ export default  {
 </template>
 
 <style scoped>
+
+.review-border {
+    background-color: transparent;
+    border: 1px solid dimgray;
+}
+
+@media only screen and (min-width: 768px) {
+    /* Apply styles for screens 768px and above */
+    .review-medium {
+        position: relative;
+        left: 20%;
+    }
+}
 
 </style>
