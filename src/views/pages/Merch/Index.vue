0edc29814f8b3f70c5d0190c3b584cdcad7bd459
Lazar Ćelić <celic96@gmail.com>
2024-04-02 00:47:37 +0200

    z-index value removed from text container

--- src/views/pages/Merch/Index.vue
+++ src/views/pages/Merch/Index.vue
@@ -15,10 +15,10 @@ export default {
     <div class="flex flex-col py-12 border-t border-white leading-2">
         <div class="">
             <div class="w-full brightness-50">
-                <img class="w-full z-10 h-52 sm:h-1/3" src="/images/home_merch.jpg">
+                <img class="w-full h-52 sm:h-1/3" src="/images/home_merch.jpg">
             </div>
         </div>
-        <div class="absolute w-full top-56 md:top-72 lg:top-96 z-20 text-center text-gray-300 md:text-2xl lg:text-5xl font-heading uppercase">
+        <div class="absolute w-full top-56 md:top-72 lg:top-96 text-center text-gray-300 md:text-2xl lg:text-5xl font-heading uppercase">
             <span>Coming</span>
             <br>
             <span>Soon</span>
